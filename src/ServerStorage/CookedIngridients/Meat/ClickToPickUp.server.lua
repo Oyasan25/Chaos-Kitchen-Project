27871cc8e90f26427a2bbfc1bf60786e6b77e16c
Oyasan25 <bencedeak06@gmail.com>
2024-08-23 03:51:41 +0200

    Clear Folders

--- src/ServerStorage/CookedIngridients/Meat/ClickToPickUp.server.lua
+++
@@ -1,6 +0,0 @@
-local Tool = script.Parent
-local Detector = Tool.ClickDetector
-
-Detector.MouseClick:Connect(function(player)
-	Tool.Parent = player.Backpack	
-end)
--- src/client/Highlighter.client.lua
+++ src/client/Highlighter.client.lua
@@ -1,21 +1,24 @@
 local Player = game.Players.LocalPlayer
 local Mouse = Player:GetMouse()
+local Highlight = Instance.new("Highlight")
+
+Highlight.FillTransparency = 1
 
 local CollectionService = game:GetService("CollectionService")
 
 Mouse.Move:Connect(function()
 	if not Mouse.Target then
-		script.Highlight.Adornee = nil
+		Highlight.Adornee = nil
 		return
 	end
 
 	if CollectionService:HasTag(Mouse.Target, "Highlightable") then
-		script.Highlight.Adornee = Mouse.Target
+		Highlight.Adornee = Mouse.Target
 		return
 	elseif CollectionService:HasTag(Mouse.Target.Parent, "Highlightable") then
-		script.Highlight.Adornee = Mouse.Target.Parent
+		Highlight.Adornee = Mouse.Target.Parent
 		return
 	end
 
-	script.Highlight.Adornee = nil
+	Highlight.Adornee = nil
 end)
